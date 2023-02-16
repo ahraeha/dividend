c3038566af38bfdeadf54e76e22914420407ec8c
jigi <ahraeha@naver.com>
2023-02-16 14:34:31 +0900

    - API, 회사명 조회 및 페이징처리

--- src/main/java/com/dayone/web/CompanyController.java
+++ src/main/java/com/dayone/web/CompanyController.java
@@ -1,8 +1,11 @@
 package com.dayone.web;
 
 import com.dayone.model.Company;
+import com.dayone.persist.entity.CompanyEntity;
 import com.dayone.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,20 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyController {
 
   private final CompanyService companyService;
+
   @GetMapping("/autocomplete")
   public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
     return null;
   }
 
   @GetMapping
-  public ResponseEntity<?> searchCompany() {
-    return null;
+  public ResponseEntity<?> searchCompany(final Pageable pageable) {
+    Page<CompanyEntity> companies = this.companyService.getAllCompany(pageable);
+    return ResponseEntity.ok(companies);
   }
 
   @PostMapping
   public ResponseEntity<?> addCompany(@RequestBody Company request) {
     String ticker = request.getTicker().trim();
-    if (ObjectUtils.isEmpty(ticker)){
+    if (ObjectUtils.isEmpty(ticker)) {
       throw new RuntimeException("ticker is Empty");
     }
 
